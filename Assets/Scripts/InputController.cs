74e7e2856c382ab647d9f4dd0fe552f4d859a4ed
Nakoren <minenkov_vladimir2003@inbox.ru>
2025-06-08 20:44:07 +0300

    Added observer object and input system

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Observer observer;
+    [SerializeField] InputActionAsset actionAsset;
+
+    private InputActionMap m_controlsMap;
+    private InputActionMap m_uiMap;
+
+    private InputAction m_moveAction;
+    private InputAction m_rotateAction;
+
+    private void Awake()
     {
-        
+        m_uiMap = actionAsset.FindActionMap("UiControls");
+        m_controlsMap = actionAsset.FindActionMap("CharacterControls");
+
+        m_moveAction = m_controlsMap.FindAction("Move");
+        m_rotateAction = m_controlsMap.FindAction("CameraMove");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        m_controlsMap.Disable();
+        m_uiMap.Disable();
+    }
+
+    private void Start()
+    {
+        m_controlsMap.Enable();
+        m_uiMap.Enable();
+    }
+
+    private void Update()
+    {
+        MoveObserver();
+        RotateObserver();
+    }
+
+    private void MoveObserver()
+    {
+        Vector3 movementVector = m_moveAction.ReadValue<Vector3>();
+        observer.Move(movementVector);
+    }
+
+    private void RotateObserver()
+    {
+        Vector3 rotationVector = m_rotateAction.ReadValue<Vector2>();
+        observer.RotateCamera(rotationVector);
     }
 }
--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Observer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int speed = 10;
+    [SerializeField] int cameraSensitivity = 10;
+
+    private CharacterController m_characterController;
+
+    private float m_xRotation = 0f;
+    private float m_yRotation = 0f;
+
+    private Vector3 m_previousPosition;
+
+    public Action<Vector3> onPositionChange;
+
+     private void Start()
     {
-        
+        m_characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RotateCamera(Vector2 rotatingVector)
     {
-        
+        m_xRotation += rotatingVector.x * cameraSensitivity * Time.deltaTime;
+        m_yRotation -= rotatingVector.y * cameraSensitivity * Time.deltaTime;
+
+        m_yRotation = Mathf.Clamp(m_yRotation, -90f, 90f);
+        transform.rotation = Quaternion.Euler(m_yRotation, m_xRotation, 0f);
     }
+
+    public void Move(Vector3 direction)
+    {
+        Vector3 forwardMovement = transform.forward * direction.z;
+        Vector3 horizontalMovement = transform.right * direction.x;
+        Vector3 verticalMovement = Vector3.up * direction.y;
+
+        Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement + verticalMovement, 1);
+        m_characterController.Move(movement * Time.deltaTime * speed);
+
+        if(transform.position - m_previousPosition != Vector3.zero)
+        {
+            if (onPositionChange == null) return;
+            onPositionChange.Invoke(transform.position);
+            m_previousPosition = transform.position;
+        }
+    }
+
+    
 }
